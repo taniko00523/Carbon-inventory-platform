2794484c9897305fc225b420901e1435eae60f65
taniko00523 <taniko00523@twmyshine.com>
2023-11-03 16:51:56 +0800

    新增CSS及修改Bug
    Bug1 文字錯誤
    Bug2 新來乍到LinkTitle指向錯誤修改
    Bug3 溫故知新內文換行修改
    新增CSS 以修改輪播Btn大小

---
+++ Data/Migrations/20231103052736_0.0.1-8.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Carbon_inventory_platform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _0018 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AreaDevice");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_AreaId",
+                table: "Devices",
+                column: "AreaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_Areas_AreaId",
+                table: "Devices",
+                column: "AreaId",
+                principalTable: "Areas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_Areas_AreaId",
+                table: "Devices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Devices_AreaId",
+                table: "Devices");
+
+            migrationBuilder.CreateTable(
+                name: "AreaDevice",
+                columns: table => new
+                {
+                    AreaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AreasId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AreaDevice", x => new { x.AreaId, x.AreasId });
+                    table.ForeignKey(
+                        name: "FK_AreaDevice_Areas_AreasId",
+                        column: x => x.AreasId,
+                        principalTable: "Areas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AreaDevice_Devices_AreaId",
+                        column: x => x.AreaId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AreaDevice_AreasId",
+                table: "AreaDevice",
+                column: "AreasId");
+        }
+    }
+}
