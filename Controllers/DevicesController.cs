ee0bcf32392f453453d9c1271173ed43cc7e2fe2
taniko00523 <taniko00523@twmyshine.com>
2023-11-16 13:58:57 +0800

    新增排放量總量計算
    新增排放量總量計算

--- Controllers/DevicesController.cs
+++ Controllers/DevicesController.cs
@@ -25,6 +25,10 @@ namespace Carbon_inventory_platform.Controllers
         string[] emissionPattern = { "固定", "移動", "製程", "逸散" };
         string[] name = { 
             "緊急發電機", "廚房", "公務車", "堆高機", "冷氣機", "冰箱", "乾燥機", "飲水機", "冰水主機", "車用空調", "工業冷媒", "CO2滅火器", "海龍滅火器", "FM200", "WD40", "化糞池", "瓦斯罐", "電力", "其他" };
+        string[] unit = { "公噸", "公秉", "千立方公尺", "千度", "人", "其他" };
+        string[] source = { "發票", "領用單", "紀錄表", "繳費單" };
+        string[] level = { "連續監測", "定期採樣", "自行評估" };
+        string[] correction = { "每年外校一次以上量測", "每年外校不到一次量測", "非量測所得知數據" };
 
         //GET: Devices
         public async Task<IActionResult> Index()
@@ -278,11 +282,6 @@ namespace Carbon_inventory_platform.Controllers
             {
                 return View();
             }
-            string[] unit = { "公噸", "公秉", "千立方公尺", "千度", "人小時", "其他" };
-            string[] source = { "發票", "領用單", "紀錄表", "繳費單" };
-            string[] level = { "連續監測", "定期採樣", "自行評估" };
-            string[] correction = { "每年外校一次以上量測", "每年外校不到一次量測", "非量測所得知數據" };
-
             ViewData["Unit"] = new SelectList(unit);
             ViewData["Source"] = new SelectList(source);
             ViewData["Level"] = new SelectList(level);
@@ -299,7 +298,10 @@ namespace Carbon_inventory_platform.Controllers
             float all = 0;
             if (toUpdate.CO2 == true && toUpdate.CH4 == true && toUpdate.N2O == true) //固定移動
             {
-                var materialsData = await _context.Materials.Where(x => x.Name == toUpdate.Material).FirstOrDefaultAsync();// 抓取對應的Materials 數據
+                var materialsData = await _context.Materials
+                    .Where(x => x.Name == toUpdate.Material)
+                    .Where(x => x.EmissionPattern == toUpdate.EmissionPattern) //區分固定與移動
+                    .FirstOrDefaultAsync();// 抓取對應的Materials 數據
                 float CH4_GWP = (float)(await _context.GWPs.Where(x => x.Name == "CH4").FirstOrDefaultAsync()).Num;
                 float N2O_GWP = (float)(await _context.GWPs.Where(x => x.Name == "N2O").FirstOrDefaultAsync()).Num;
 
@@ -355,12 +357,12 @@ namespace Carbon_inventory_platform.Controllers
                 }
             } else if (toUpdate.CH4 == true)
             {
-                var materialsData = await _context.Materials.Where(x => x.Name == toUpdate.Material).FirstOrDefaultAsync();// 抓取對應的Materials 數據
+                var GWPData = await _context.GWPs.Where(x => x.Name == toUpdate.Material).FirstOrDefaultAsync();
 
                 // 如果找到 Materials 数据，计算 Emissions
-                if (materialsData != null)
+                if (GWPData != null)
                 {
-                    all = (float)(activityData.Num * materialsData.CO2CEF);
+                    all = (float)(activityData.Num * 0.002546062 * GWPData.Num);
                 }
             }
 
@@ -377,10 +379,7 @@ namespace Carbon_inventory_platform.Controllers
                 toUpdate.Emissions = all;
                 //toUpdate.ModifiedTime = DateTime.Now;
             }
-            string[] unit = { "公噸", "公秉", "千立方公尺", "千度", "人小時", "其他" };
-            string[] source = { "發票", "領用單", "紀錄表", "繳費單" };
-            string[] level = { "連續監測", "定期採樣", "自行評估" };
-            string[] correction = { "每年外校一次以上量測", "每年外校不到一次量測", "非量測所得知數據" };
+           
 
             ViewData["Unit"] = new SelectList(unit);
             ViewData["Source"] = new SelectList(source);
---
+++ Controllers/EmissionController.cs
@@ -0,0 +1,60 @@
+﻿using Carbon_inventory_platform.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Carbon_inventory_platform.Controllers
+{
+    public class EmissionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmissionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            float sum_hardlymove = 0;
+            float sum_move = 0;
+            float sum_escape = 0;
+            float sum_process = 0;
+            float sum_electricity = 0;
+
+            foreach (var item in await _context.Devices.ToListAsync())
+            {
+                if(item.EmissionPattern == "固定")
+                {
+                    sum_hardlymove = sum_hardlymove + item.Emissions;
+                } else if (item.EmissionPattern == "移動")
+                {
+                    sum_move = sum_move + item.Emissions;
+                }
+                else if (item.EmissionPattern == "逸散")
+                {
+                    sum_escape = sum_escape + item.Emissions;
+                }
+                else if (item.EmissionPattern == "移動")
+                {
+                    sum_process = sum_process + item.Emissions;
+                }
+                else if (item.EmissionPattern == "外購電力")
+                {
+                    sum_electricity = sum_electricity + item.Emissions;
+                }
+            }
+            ViewData["sum_hardlymove"] = sum_hardlymove;
+            ViewData["sum_move"] = sum_move;
+            ViewData["sum_escape"] = sum_escape;
+            ViewData["sum_process"] = sum_process;
+            ViewData["sum_electricity"] = sum_electricity;
+
+            return _context.Devices != null ? //如果有抓到資料表Null
+                          View(await _context.Devices
+                          .Where(x => x.isDeleted == 0) //抓出資料表裡面沒被刪除的
+                          .OrderBy(x => x.CreateTime)
+                          .Include(x => x.Areas)
+                          .ToListAsync()) : //非同步方法
+                          Problem("沒有找到資料表"); //否則回報問題 Entity set 'ApplicationDbContext.Companies'  is null.
+        }
+    }
+}
