9d9d4c70a03d675947d522e35e99256955733e8f
taniko00523 <taniko00523@twmyshine.com>
2023-11-27 01:40:00 +0800

    修改新增設備畫面
    修改新增設備畫面，排放源新增「其他」選項，並提供輸入空格填寫

--- Controllers/DevicesController.cs
+++ Controllers/DevicesController.cs
@@ -317,7 +317,11 @@ namespace Carbon_inventory_platform.Controllers
                 return Problem("沒有找到資料");
             }
             var toDelete = await _context.Devices.FindAsync(id);
-            if (toDelete != null)
+            if(toDelete.Num == 0)
+            {
+                _context.Devices.Remove(toDelete);
+            }
+            else if (toDelete != null)
             {
                 toDelete.isDeleted = 1;
                 toDelete.DeleteTime = DateTime.Now;
