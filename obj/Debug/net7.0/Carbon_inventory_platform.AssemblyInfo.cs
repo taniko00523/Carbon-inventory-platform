3ce4c82b11a3bacffceca08ecab109d4f6fb7aa9
taniko00523 <taniko00523@twmyshine.com>
2024-04-05 19:22:19 +0800

    畫面修改
    將排放源新增畫面進行修改

--- obj/Debug/net7.0/Carbon_inventory_platform.AssemblyInfo.cs
+++ obj/Debug/net7.0/Carbon_inventory_platform.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Carbon_inventory_platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b55f8c996a7bfa27c61e35dba40922fda177459")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04d80a952c2fd1507a8405e63dd17434805f2df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Carbon_inventory_platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Carbon_inventory_platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
