6369cd8b9b803198b8e9a8728fdd5b887c696c5d
taniko00523 <taniko00523@twmyshine.com>
2024-08-08 22:47:34 +0800

    刪除算總排放量Function 新增AR5、6版本下載畫面

--- Controllers/EmissionController.cs
+++ Controllers/EmissionController.cs
@@ -36,6 +36,20 @@ namespace Carbon_inventory_platform.Controllers
 
             return View(emissions);
         }
+        public async Task<IActionResult> AR5ReportAsync(Guid? id)
+        {
+            // 跳轉到Area修改AR版本
+            // 完後跳到Word3Aync
+            var areaId = TempData.Peek("areaId");
+            return RedirectToAction("Index", "Devices", new { id = areaId });
+        } 
+        public async Task<IActionResult> AR6ReportAsync(Guid? id)
+        {
+            // 跳轉到Area修改AR版本
+            // 完後跳到Word3Aync
+            var areaId = TempData.Peek("areaId");
+            return RedirectToAction("Index", "Devices", new { id = areaId });
+        }
         public async Task<IActionResult> ChartAsync(Guid? id)
         {
             TempData["yearId"] = id;
@@ -49,235 +63,7 @@ namespace Carbon_inventory_platform.Controllers
             return View(emissions);
 
         }
-        public async Task<bool> CountEmissionAsync(Guid? id)
-        {
-            // 合并数据库查询
-            var areaData = await _context.Areas
-    .Include(a => a.Devices.Where(d => d.isDeleted == 0))  // 只包含 isDeleted == 0 的 Devices
-    .ThenInclude(d => d.GHGs)
-    .Where(a => a.Id == id && a.isDeleted == 0)  // Area 的 Id 等于 id，且 IsDeleted == 0
-    .FirstOrDefaultAsync();
-
-
-            if (areaData == null) return false;
-            if (areaData.Devices == null) return false;
-            // 初始化所有需要的变量
-            decimal sumHardlyMove = 0, sumMove = 0, sumEscape = 0, sumProcess = 0, sumElectricity = 0;
-            decimal sum1_CO2 = 0, sum2_CO2 = 0, sum1_CH4 = 0, sum2_CH4 = 0, sum1_N2O = 0, sum2_N2O = 0;
-            decimal sum1_HFCS = 0, sum2_HFCS = 0, sum1_PFCS = 0, sum2_PFCS = 0, sum1_SF6 = 0, sum2_SF6 = 0, sum1_NF3 = 0, sum2_NF3 = 0;
-            decimal sumScope1 = 0, sumScope2 = 0, sumAll = 0;
-            int no1Grade = 0, no2Grade = 0, no3Grade = 0;
-            float avgGrade = 0;
-            decimal all_UUL = 0, all_ULL = 0, allCountUUL = 0, allCountULL = 0, sumUncertainty = 0;
-
-            // 計算排放量及
-            foreach (var device in areaData.Devices)
-            {
-                if (device.GHGs != null)
-                {
-                    foreach (var GHG in device.GHGs)
-                    {
-                        if (device.Scope == "類別一")
-                        {
-                            switch (GHG.Name)
-                            {
-                                case "CO2": sum1_CO2 += GHG.Emission; break;
-                                case "CH4": sum1_CH4 += GHG.Emission; break;
-                                case "N2O": sum1_N2O += GHG.Emission; break;
-                                case "HFCS": sum1_HFCS += GHG.Emission; break;
-                                case "PFCS": sum1_PFCS += GHG.Emission; break;
-                                case "SF6": sum1_SF6 += GHG.Emission; break;
-                                case "NF3": sum1_NF3 += GHG.Emission; break;
-                            }
-                        }
-                        else if (device.Scope == "類別二")
-                        {
-                            switch (GHG.Name)
-                            {
-                                case "CO2": sum2_CO2 += GHG.Emission; break;
-                                case "CH4": sum2_CH4 += GHG.Emission; break;
-                                case "N2O": sum2_N2O += GHG.Emission; break;
-                                case "HFCS": sum2_HFCS += GHG.Emission; break;
-                                case "PFCS": sum2_PFCS += GHG.Emission; break;
-                                case "SF6": sum2_SF6 += GHG.Emission; break;
-                                case "NF3": sum2_NF3 += GHG.Emission; break;
-                            }
-                        }
-                    }
-                }
-               
-                switch (device.EmissionPattern)
-                {
-                    case "固定": sumHardlyMove += device.Emissions; break;
-                    case "移動": sumMove += device.Emissions; break;
-                    case "逸散": sumEscape += device.Emissions; break;
-                    case "製程": sumProcess += device.Emissions; break;
-                    case "外購電力": sumElectricity += device.Emissions; break;
-                }
-
-                if (device.Grade < 10 && device.Grade > 0) no1Grade++;
-                else if (device.Grade < 19) no2Grade++;
-                else no3Grade++;
-
-                allCountULL += device.count_ULL;
-                allCountUUL += device.count_UUL;
-
-                if (device.all_ULL != 0)
-                {
-                    sumUncertainty += device.Emissions;
-                }
-            }
-
-            decimal CO2 = sum1_CO2 + sum2_CO2;
-            decimal CH4 = sum1_CH4 + sum2_CH4;
-            decimal N2O = sum1_N2O + sum2_N2O;
-            decimal HFCS = sum1_HFCS + sum2_HFCS;
-            decimal PFCS = sum1_PFCS + sum2_PFCS;
-            decimal SF6 = sum1_SF6 + sum2_SF6;
-            decimal NF3 = sum1_NF3 + sum2_SF6;
-
-            sumScope1 = sum1_CO2 + sum1_CH4 + sum1_N2O + sum1_HFCS + sum1_PFCS + sum1_NF3 + sum1_SF6;
-            sumScope2 = sum2_CO2 + sum2_CH4 + sum2_N2O + sum2_HFCS + sum2_PFCS + sum2_NF3 + sum2_SF6;
-            sumAll = sumScope1 + sumScope2;
-
-            foreach (var device in areaData.Devices)
-            {
-                avgGrade += (float)Math.Round((float)(Math.Round(device.Emissions / sumAll, 4) * device.Grade), 2);
-            }
-
-            if (sumUncertainty != 0)
-            {
-                all_UUL = DecimalSqrt(allCountUUL) / sumUncertainty;
-                all_ULL = DecimalSqrt(allCountULL) / sumUncertainty;
-            }
-
-            bool sumMatch = areaData.All != Math.Round(sumAll, 3);
-            bool uncertaintyMatch = areaData.ULL != Math.Round(all_ULL, 2);
-
-            if (sumMatch || uncertaintyMatch)
-            {
-                // 更新資料庫中的資料
-                UpdateEmissionData(areaData, sum1_CO2, sum1_CH4, sum1_N2O, sum1_HFCS, sum1_PFCS, sum1_SF6, sum1_NF3,
-                    sum2_CO2, sum2_CH4, sum2_N2O, sum2_HFCS, sum2_PFCS, sum2_SF6, sum2_NF3,
-                    CO2, CH4, N2O, HFCS, PFCS, SF6, NF3,
-                    sumScope1, sumScope2, sumAll,
-                    sumHardlyMove, sumMove, sumEscape, sumProcess,
-                    all_UUL, all_ULL, sumUncertainty,
-                    no1Grade, no2Grade, no3Grade, avgGrade);
-
-                await _context.SaveChangesAsync();
-            }
-
-            return true;
-        }
-        private void UpdateEmissionData(Area areaData,
-            decimal sum1_CO2, decimal sum1_CH4, decimal sum1_N2O, decimal sum1_HFCS, decimal sum1_PFCS, decimal sum1_SF6, decimal sum1_NF3,
-            decimal sum2_CO2, decimal sum2_CH4, decimal sum2_N2O, decimal sum2_HFCS, decimal sum2_PFCS, decimal sum2_SF6, decimal sum2_NF3,
-            decimal CO2, decimal CH4, decimal N2O, decimal HFCS, decimal PFCS, decimal SF6, decimal NF3,
-            decimal sumScope1, decimal sumScope2, decimal sumAll,
-            decimal sumHardlyMove, decimal sumMove, decimal sumEscape, decimal sumProcess,
-            decimal all_UUL, decimal all_ULL, decimal sumUncertainty,
-            int no1Grade, int no2Grade, int no3Grade, float avgGrade)
-        {
-            areaData.Scope1_CO2 = sum1_CO2;
-            areaData.Scope1_CH4 = sum1_CH4;
-            areaData.Scope1_N2O = sum1_N2O;
-            areaData.Scope1_HFCS = sum1_HFCS;
-            areaData.Scope1_PFCS = sum1_PFCS;
-            areaData.Scope1_SF6 = sum1_SF6;
-            areaData.Scope1_NF3 = sum1_NF3;
-
-            areaData.Scope2_CO2 = sum2_CO2;
-            areaData.Scope2_CH4 = sum2_CH4;
-            areaData.Scope2_N2O = sum2_N2O;
-            areaData.Scope2_HFCS = sum2_HFCS;
-            areaData.Scope2_PFCS = sum2_PFCS;
-            areaData.Scope2_SF6 = sum2_SF6;
-            areaData.Scope2_NF3 = sum2_NF3;
-
-            areaData.CO2 = CO2;
-            areaData.CH4 = CH4;
-            areaData.N2O = N2O;
-            areaData.HFCS = HFCS;
-            areaData.PFCS = PFCS;
-            areaData.SF6 = SF6;
-            areaData.NF3 = NF3;
-
-            areaData.Scope1 = sumScope1;
-            areaData.Scope2 = sumScope2;
-            areaData.All = sumAll;
-
-            areaData.non_move = sumHardlyMove;
-            areaData.move = sumMove;
-            areaData.escape = sumEscape;
-            areaData.process = sumProcess;
-
-            if (sumScope1 != 0)
-            {
-                areaData.percentage1_CO2 = (sum1_CO2 / sumScope1 * 100);
-                areaData.percentage1_CH4 = (sum1_CH4 / sumScope1 * 100);
-                areaData.percentage1_N2O = (sum1_N2O / sumScope1 * 100);
-                areaData.percentage1_HFCS = (sum1_HFCS / sumScope1 * 100);
-                areaData.percentage1_PFCS = (sum1_PFCS / sumScope1 * 100);
-                areaData.percentage1_SF6 = (sum1_SF6 / sumScope1 * 100);
-                areaData.percentage1_NF3 = (sum1_NF3 / sumScope1 * 100);
-            }
-            else
-            {
-                areaData.percentage1_CO2 = 0;
-                areaData.percentage1_CH4 = 0;
-                areaData.percentage1_N2O = 0;
-                areaData.percentage1_HFCS = 0;
-                areaData.percentage1_PFCS = 0;
-                areaData.percentage1_SF6 = 0;
-                areaData.percentage1_NF3 = 0;
-            }
-
-            if (sumAll != 0)
-            {
-                areaData.percentage2_CO2 = (CO2 / sumAll * 100);
-                areaData.percentage2_CH4 = (CH4 / sumAll * 100);
-                areaData.percentage2_N2O = (N2O / sumAll * 100);
-                areaData.percentage2_HFCS = (HFCS / sumAll * 100);
-                areaData.percentage2_PFCS = (PFCS / sumAll * 100);
-                areaData.percentage2_SF6 = (SF6 / sumAll * 100);
-                areaData.percentage2_NF3 = (NF3 / sumAll * 100);
-
-                areaData.percentage_nonMove = (sumHardlyMove / sumAll * 100);
-                areaData.percentage_Move = (sumMove / sumAll * 100);
-                areaData.percentage_Escape = (sumEscape / sumAll * 100);
-                areaData.percentage_Process = (sumProcess / sumAll * 100);
-                areaData.percentage_Scope1 = (sumScope1 / sumAll * 100);
-                areaData.percentage_Scope2 = (sumScope2 / sumAll * 100);
-            }
-            else
-            {
-                areaData.percentage2_CO2 = 0;
-                areaData.percentage2_CH4 = 0;
-                areaData.percentage2_N2O = 0;
-                areaData.percentage2_HFCS = 0;
-                areaData.percentage2_PFCS = 0;
-                areaData.percentage2_SF6 = 0;
-                areaData.percentage2_NF3 = 0;
-
-                areaData.percentage_nonMove = 0;
-                areaData.percentage_Move = 0;
-                areaData.percentage_Escape = 0;
-                areaData.percentage_Process = 0;
-                areaData.percentage_Scope1 = 0;
-                areaData.percentage_Scope2 = 0;
-            }
-
-            areaData.cal_all = sumUncertainty;
-            areaData.no1_Grade = no1Grade;
-            areaData.no2_Grade = no2Grade;
-            areaData.no3_Grade = no3Grade;
-            areaData.avg_Grade = avgGrade;
-            areaData.all_Grade = avgGrade < 10 ? "第一級" : (avgGrade < 19 ? "第二級" : "第三級");
-            areaData.percentage_CalAll = (sumUncertainty / sumAll * 100);
-            areaData.ULL = all_ULL;
-            areaData.UUL = all_UUL;
-        }
+        
         public async Task<IActionResult> Word3Async(Guid id)
         {
             bool success = await CountEmissionAsync(id);
